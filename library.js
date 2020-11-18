470f7f4e242176d7a19e0b15495bde59982354f4
Chris Webb <Christopher-webb@live.co.uk>
2020-11-18 00:08:57 +0000

    made changes to collect form data, button is now submit

--- library.js
+++ library.js
@@ -1,12 +1,13 @@
 let newBookBtn = document.getElementById('newBookBtn');
 let submitBtn = document.getElementById('submitBtn');
 let yourBooksBar = document.getElementById('libraryTable')
-let mainContainer = document.getElementById('mainContainer');   // not needed I don't think
+let mainContainer = document.getElementById('mainContainer');
+
 let isFormOpen = false; 
 let myLibrary = [];
 
 newBookBtn.addEventListener("click", popOutForm);
-submitBtn.addEventListener("click", addNewBook);
+//submitBtn.addEventListener("click", addNewBook);
 
 let book1 = {
     title: "The Way Of Kings",
@@ -25,28 +26,36 @@ let book2 = {
 myLibrary.push(book1, book2);
 console.log(myLibrary);
 
+const form = document.querySelector('#myForm')
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(e.target.elements);
+  const title = e.target.elements.book_title.value
+  const author = e.target.elements.book_author.value
+  const pages = e.target.elements.book_pages.value
+  const read = e.target.elements.book_read.value
+  console.log(title, author, pages, read)
+  addNewBook(title, author, pages, read);
+})
+
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
-        return `The ${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+        return `The ${this.title} written by ${this.author}, is ${this.pages} pages, ${this.read}`
     };
+    console.log(info())
 };
 
-function popOutForm () {
-    isFormOpen = true;
-    if (isFormOpen === true) {
-        openForm()
-    };
-};
+function addNewBook(title, author, pages, read) {
+    let book = new Book ({
 
-function addNewBook() {
-    // when submit book button is pressed, save all form data to a new object
-    // then push the book to the myLibraryArray and then invoke the display Library function
+    });
+    myLibrary.push(book);
 };
-
 
 function displayLibrary () {
     myLibrary.forEach(function (book) {
@@ -56,8 +65,15 @@ function displayLibrary () {
             read: ${book.read}`;
         mainContainer.appendChild(bookDiv);
     });
+    // displayLibrary();
 };
 
+function popOutForm () {
+    isFormOpen = true;
+    if (isFormOpen === true) {
+        openForm()
+    };
+};
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
@@ -67,5 +83,4 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
 
-  
 displayLibrary();
